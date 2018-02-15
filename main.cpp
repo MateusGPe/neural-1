cf5008a67551fc867f0deb2aeb5551879a209230
2pac <tarasyaremabcn@gmail.com>
2018-02-15 11:02:45 +0100

    ...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 
 // Structure for input and output layers
 struct OutLayer {
@@ -21,27 +22,81 @@ struct Brain {
 	HiddenLayer *hidden;
 };
 
-void initVector(int n, double **vec) {
-	double *v;
-	v = (double *)malloc(n * sizeof(double));
+void initVectorDouble(int n, double **vec) {
+	double *v = (double *)malloc(n * sizeof(double));
 	if(v == NULL) exit(1);
 	*vec=v;
 }
 
-void initMatrix(int n, int m, double ***a) {
-	double **mat;
-  	mat = (double **)malloc(n * sizeof(double *));
+void initVectorInt(int n, int **vec) {
+	int *v = (int *)malloc(n * sizeof(int));
+	if(v == NULL) exit(1);
+	*vec=v;
+}
+
+void initMatrixDouble(int n, int m, double ***a) {
+	double **mat = (double **)malloc(n * sizeof(double *));
   	if(mat == NULL) exit(1);
   	for(int i = 0; i < n; ++i) {
-    	mat[i] = (double *)calloc(m * sizeof(double));
+    	mat[i] = (double *)malloc(m * sizeof(double));
     	if(mat[i] == NULL) exit(1);
   	}
   	*a = mat;
 }
 
-void freeMatrix(int n, double **mat) {
+void freeMatrixDouble(int n, double **mat) {
 	for(int i = 0; i < n; ++i) free(mat[i]);
   	free(mat);
+}
+
+double randomDouble() {
+	return (double)rand()/RAND_MAX;
+}
+
+double activationFunction(double x) {
+	return 1. / (1. + exp(-x)); 
+}
+
+void sumHiddenInput(int nH, int n, int m, HiddenLayer *h) {
+	for(int i = 0; i < n; ++i) {
+		h[nH].node[i] = 0;
+		for(int j = 0; j < m; ++j)
+			h[nH].node[i] += h[nH].inEdge[j][i];
+		// h[nH].node[i] = activationFunction(h[nH].node[i]);
+	}
+}
+
+void sumHiddenOutput(int nH, int n, int nO, HiddenLayer *h, OutLayer o) {
+	for(int i = 0; i < nO; ++i) {
+		o.node[i] = 0;
+		for(int j = 0; j < nO; ++j)
+			o.node[i] += h[nH].node[j] * h[nH].outEdge[j][i];
+		// o.node[i] = activationFunction(o.node[i]);
+	}
+}
+
+void randomMatrix(int n, int m, double **M) {
+	for(int i = 0; i < n; ++i)
+		for(int j = 0; j < m; ++j)
+			M[i][j] = randomDouble();
+}
+
+void printVector(int n, double *v) {
+	for(int i = 0; i < n; ++i) printf("%0.2lf\t", v[i]);
+	printf("\n");
+}
+
+void printMatrix(int n, int m, double **M) {
+	for(int i = 0; i < m; ++i)
+		printf("\t%d", i + 1);
+	printf("\n");
+
+	for(int i = 0; i < n; ++i) {
+		printf("%d", i + 1);
+		for(int j = 0; j < m; ++j)
+			printf("\t%0.2lf", M[i][j]);
+		printf("\n");
+	}
 }
 
 int main() {
@@ -50,24 +105,41 @@ int main() {
 	
 	// Specific values for the network
 	brain.numInputNodes = 2;
-	brain.numOutputNodes = 2;
+	brain.numOutputNodes = 1;
 	brain.numHiddenLayers = 1;
 
 	// Allocating memory for the neural network
 	// check of NULL pointer TODO
-	initVector(brain.numHiddenLayers, brain.nodesHidden);
-	for(int i = 0; i < brain.numHiddenLayers; ++i) brain.nodesHidden[i] = 1;
+	initVectorInt(brain.numHiddenLayers, &brain.nodesHidden);
+	for(int i = 0; i < brain.numHiddenLayers; ++i) brain.nodesHidden[i] = 3;
 	
-	initVector(numInputNodes, brain.input.node);
-	initVector(numOutputNodes, brain.output.node);
+	initVectorDouble(brain.numInputNodes, &brain.input.node);
+	initVectorDouble(brain.numOutputNodes, &brain.output.node);
 	
-	brain.hidden = (HiddenLayer *)calloc(numHiddenLayers * sizeof(HiddenLayer));
-	for(int i = 0; i < numHiddenLayers; ++i) {
+	brain.hidden = (HiddenLayer *)malloc(brain.numHiddenLayers * sizeof(HiddenLayer));
+	for(int i = 0; i < brain.numHiddenLayers; ++i) {
 		if(i == 0) {
-			initVector(brain.nodesHidden[i], brain.hidden[i].node);
-			initMatrix(brain.nodesHidden[i], brain.numInputNodes, brain.hidden[i].inEdge);
-			initMAtrix(brain.nodesHidden[i], brain.numOutputNodes, brain.hidden[i].outEdge);	
+			initVectorDouble(brain.nodesHidden[i], &brain.hidden[i].node);
+			initMatrixDouble(brain.nodesHidden[i], brain.numInputNodes, &brain.hidden[i].inEdge);
+			initMatrixDouble(brain.nodesHidden[i], brain.numOutputNodes, &brain.hidden[i].outEdge);	
 		}
+	}
+
+	// Some tests
+	srand(time(NULL));
+
+	for(int i = 0; i < brain.numHiddenLayers; ++i) {
+		randomMatrix(brain.nodesHidden[i], brain.numInputNodes, brain.hidden[i].inEdge);
+		randomMatrix(brain.nodesHidden[i], brain.numOutputNodes, brain.hidden[i].outEdge);
+
+		printMatrix(brain.nodesHidden[i], brain.numInputNodes, brain.hidden[i].inEdge);
+		printMatrix(brain.nodesHidden[i], brain.numOutputNodes, brain.hidden[i].outEdge);
+
+		sumHiddenInput(i, brain.nodesHidden[i], brain.numInputNodes, &brain.hidden[i]);
+		printVector(brain.nodesHidden[i], brain.hidden[i].node);
+
+		sumHiddenOutput(i, brain.nodesHidden[i], brain.numOutputNodes, &brain.hidden[i], brain.output);
+		printVector(brain.numOutputNodes, brain.output.node);
 	}
 
 
@@ -77,10 +149,10 @@ int main() {
 	// Free of the neural network
 	free(brain.input.node);
 	free(brain.output.node);
-	for(int i = 0; i < numHiddenLayers; ++i) {
+	for(int i = 0; i < brain.numHiddenLayers; ++i) {
 		free(brain.hidden[i].node);
-		freeMatrix(brain.nodesHidden[i], brain.hidden[i].inEdge);
-		freeMAtrix(brain.nodesHidden[i], brain.hidden[i].outEdge);	
+		freeMatrixDouble(brain.nodesHidden[i], brain.hidden[i].inEdge);
+		freeMatrixDouble(brain.nodesHidden[i], brain.hidden[i].outEdge);	
 	}
 	free(brain.hidden);
 
