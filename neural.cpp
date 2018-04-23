dd3a334c5da90ba52bf3e392ea30441ae3283891
Tarás Yarema <tarasyaremabcn@gmail.com>
2018-04-23 18:55:44 +0200

    Some changes

--- neural.cpp
+++ neural.cpp
@@ -34,24 +34,57 @@ struct Vector {
 };
 
 struct Layer {
-    Vector in, out, nodes;
+    Vector *in, nodes, *out;
 
-    void _init(unsigned int in_n, unsigned int out_n, unsigned int n) {
-        in._init(in_n);
-        out._init(out_n);
+    void _init(unsigned int in_n, unsigned int n, unsigned int out_n) {
         nodes._init(n);
+
+        in = (Vector *)malloc(in_n * sizeof(Vector));
+        out = (Vector *)malloc(out_n * sizeof(Vector));
+        if (in == NULL || out == NULL) {
+            exit(1);
+        }
+
+        for (int i = 0; i < in_n; ++i) {
+            in[i]._init(in_n);
+        }
+        for (int i = 0; i < out_n; ++i) {
+            out[i]._init(in_n);
+        }
     }
 
     void _free() {
-        in._free();
-        out._free();
+        if (in != NULL) {
+            for (int i = 0; i < in[0].dim; ++i) {
+                in[i]._free();
+            }
+        }
+
+        if (out != NULL) {
+            for (int i = 0; i < out[0].dim; ++i) {
+                out[i]._free();
+            }
+        }
+
         nodes._free();
     }
 
     void _print() {
-        in._print();
-        out._print();
+        if (in != NULL) {
+            for (int i = 0; i < in[0].dim; ++i) {
+                in[i]._print();
+            }
+        }
+
         nodes._print();
+        
+        if (out != NULL) {
+            for (int i = 0; i < out[0].dim; ++i) {
+                out[i]._print();
+            }
+        }
+
+        
         printf("\n");
     }
 
@@ -80,13 +113,20 @@ struct Brain {
         hid._print();
         out._print();
     }
+
+    void random() {
+        in.random_nodes();
+        hid.random_nodes();
+        out.random_nodes();
+    }
 };
 
 int main (void) {
     Brain test;
     srand(time(NULL));
 
-    test._init(3, 2, 0);
+    test._init(3, 3, 2);
+    test.random();
     test._print();
     test._free();
 
